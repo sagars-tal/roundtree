42cc1ff87d93d6746cb41a3222a6ea13cd9b6e1b
shende7 <sagar.shende7@gmail.com>
2020-08-16 23:37:39 +0530

    Updates

---
+++ precache-manifest.ae9ad2a79665ed78547435f6c76c805c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb2cc696bb424c4ad818fb92e831ded",
+    "url": "/roundtree/index.html"
+  },
+  {
+    "revision": "56e61e7407182efe701c",
+    "url": "/roundtree/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "d53ded347d381bad7c0a",
+    "url": "/roundtree/static/css/main.bcab401d.chunk.css"
+  },
+  {
+    "revision": "56e61e7407182efe701c",
+    "url": "/roundtree/static/js/2.c60ef59b.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/roundtree/static/js/2.c60ef59b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d53ded347d381bad7c0a",
+    "url": "/roundtree/static/js/main.067f58b2.chunk.js"
+  },
+  {
+    "revision": "e9374aefe164bf0a6add",
+    "url": "/roundtree/static/js/runtime-main.c8b9201f.js"
+  },
+  {
+    "revision": "3bd97c45dfd71aae576bab5165f1f592",
+    "url": "/roundtree/static/media/firstsection.3bd97c45.svg"
+  }
+]);
